2d8e324c3c94092f1be7b3da42df4990fa676a94
JonathanPodvinDesign <jonathanpodvindesign@gmail.com>
2024-05-31 09:26:25 +0200

    fix mapping model relation folder

--- app/Models/Mapping.php
+++ app/Models/Mapping.php
@@ -14,12 +14,17 @@ class Mapping extends Model
         "input_rubrique",
         "name_rubrique",
         "output_rubrique_id",
-        "output_type",
         "company_folder_id",
+        "output_type"
     ];
 
     public function output()
     {
         return $this->morphTo('output_rubrique', 'output_type', 'output_rubrique_id');
+    }
+
+    public function folder()
+    {
+        return $this->belongsTo(CompanyFolder::class, 'company_folder_id');
     }
 }
