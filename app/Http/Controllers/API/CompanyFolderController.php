0a1031ccac461909fe50da6ac9c280edb48ca002
JonathanPodvinDesign <jonathanpodvindesign@gmail.com>
2024-06-12 22:16:02 +0200

    update company folder

--- app/Http/Controllers/API/CompanyFolderController.php
+++ app/Http/Controllers/API/CompanyFolderController.php
@@ -15,6 +15,7 @@ class CompanyFolderController extends Controller
             'folder_name' => 'required|string',
             'siret' => 'required|string',
             'siren' => 'required|string',
+            'interface_id' => 'required|exists:interfaces,id',
             'company_id' => 'exists:companies,id',
         ]);
         if ($validator->fails()) {
@@ -26,6 +27,7 @@ class CompanyFolderController extends Controller
                 'folder_name' => $request->folder_name,
                 'siret' => $request->siret,
                 'siren' => $request->siren,
+                'interface_id' => $request->interface_id,
                 'company_id' => $request->company_id,
             ];
             CompanyFolder::create($data);
@@ -34,6 +36,36 @@ class CompanyFolderController extends Controller
         } catch (\Exception $e) {
             dd($e);
             return response()->json(['error' => 'Une erreur est survenue lors de la création du dossier.'], 500);
+        }
+    }
+
+    public function updateCompanyFolder(Request $request, $id){
+        $validator = Validator::make($request->all(), [
+            'folder_number' => 'required|string',
+            'folder_name' => 'required|string',
+            'siret' => 'required|string',
+            'siren' => 'required|string',
+            'interface_id' => 'required|exists:interfaces,id',
+            'company_id' => 'exists:companies,id',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+        try {
+            $data = [
+                'folder_number' => $request->folder_number,
+                'folder_name' => $request->folder_name,
+                'siret' => $request->siret,
+                'siren' => $request->siren,
+                'interface_id' => $request->interface_id,
+                'company_id' => $request->company_id,
+            ];
+            CompanyFolder::where('id', $id)->update($data);
+            return response()->json(['message' => 'Dossier modifié avec succès'], 200);
+
+        } catch (\Exception $e) {
+            dd($e);
+            return response()->json(['error' => 'Une erreur est survenue lors de la modification du dossier.'], 500);
         }
     }
 }
