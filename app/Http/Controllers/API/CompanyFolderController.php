ee98cee749c739d3686d13f69625f0b29c51cb4b
Aurélien Carteret <a.carteret@inpact.fr>
2024-06-25 18:18:50 +0200

    [WIP] - Convert

--- app/Http/Controllers/API/CompanyFolderController.php
+++ app/Http/Controllers/API/CompanyFolderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\CompanyFolder;
 use App\Models\InterfaceFolder;
+use App\Models\Mapping;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -31,22 +32,28 @@ class CompanyFolderController extends Controller
                 'folder_name' => $request->folder_name,
                 'siret' => $request->siret,
                 'siren' => $request->siren,
-                'interface_id' => $request->interface_id,
                 'company_id' => $request->company_id,
             ];
+            $existingFolder = CompanyFolder::all()->where('folder_number', '==', $request->get('folder_number'))->first();
+            if ($existingFolder){
+                return response()->json(['message' => 'Le numéro de dossier ' . $request->folder_number . ' est déjà associé à ' . $existingFolder->folder_name]);
+            }
             CompanyFolder::create($data);
 
             $folder = CompanyFolder::where('folder_number', $request->folder_number)->first();
 
-            if ($folder && $request->interface_id) {
-                $interface = InterfaceFolder::create([
-                    'company_folder_id' => $folder->id,
-                    'interface_folder_id' => $request->interface_id
-                ]);                  
-            }
+            InterfaceFolder::create([
+                'company_folder_id' => $folder->id,
+                'interface_folder_id' => $request->interface_id
+            ]);
+            Mapping::create([
+                'company_folder_id' => $folder->id,
+                'data' => [],
+            ]);
             return response()->json(['message' => 'Dossier créé avec succès'], 200);
 
         } catch (\Exception $e) {
+            dd($e);
             return response()->json(['error' => 'Une erreur est survenue lors de la création du dossier.'], 500);
         }
     }
