e5edca628b36508d3e50efc5df88eb5fe83147f7
Aurélien Carteret <a.carteret@inpact.fr>
2024-07-02 15:07:33 +0200

    [WIP] - Mapping

--- app/Http/Controllers/API/MappingController.php
+++ app/Http/Controllers/API/MappingController.php
@@ -80,7 +80,7 @@ class MappingController extends Controller
                         'base_calcul' => null,
                         'label' => null,
                         'is_mapped' => false,
-                        'is_used' => false,
+                        'is_used' => true,
                         'company_folder_id' => $companyFolder,
                     ];
                 }
